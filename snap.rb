0bf1d6b6f89a6e02c2efb72bd543f08bcf9eeb0b
Omri Gazitt <ogazitt@gmail.com>
2020-04-15 21:45:33 -0700

    updated SHA of binary

--- snap.rb
+++ snap.rb
@@ -2,7 +2,7 @@ class Snap < Formula
   desc "A command-line tool for managing snaps - workflows that connect triggers to actions"
   homepage "https://github.com/snapmaster-io/snap"
   url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap"
-  sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
+  sha256 "01228e96a5cefe333839a99bccb3c01313bd60cc8e7eda7674bde08ef4f57c45"
   version "0.1.0"
 
   bottle :unneeded
