bc57cba60206f65c0273a75e6229df4670635d8c
Omri Gazitt <ogazitt@gmail.com>
2020-04-17 12:07:02 -0700

    revision fixes snap active get bug

--- snap.rb
+++ snap.rb
@@ -2,8 +2,8 @@ class Snap < Formula
   desc "A command-line tool for managing snaps - workflows that connect triggers to actions"
   homepage "https://github.com/snapmaster-io/snap"
   url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap"
-  sha256 "c12f5a1015c10ab413a7a4a0c0d2dd402ef242c9a68e03e1fbca602b565303e1"
-  version "0.3.1"
+  sha256 "6fbc0ea28f11865282e0b97d3e1a4c08fc18ef4b814f75317081df70e360cc81"
+  version "0.3.2"
 
   bottle :unneeded
 
