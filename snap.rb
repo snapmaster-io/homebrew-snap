9af6dddc614078582671d83671ad2362d5509ba7
Omri Gazitt <ogazitt@gmail.com>
2020-04-21 23:52:05 -0700

    updated version to 0.4.0

--- snap.rb
+++ snap.rb
@@ -3,7 +3,7 @@ class Snap < Formula
   homepage "https://github.com/snapmaster-io/snap"
   url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap"
   sha256 "3a613662109e533eace3005ff8e59470b9f37fed13fb41b712ab1ae113862cd8"
-  version "0.3.2"
+  version "0.4.0"
 
   bottle :unneeded
 
