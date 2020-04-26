aa4a6c99ad86451cbe1f001517cf8b8bd9d071db
Omri Gazitt <ogazitt@gmail.com>
2020-04-25 19:37:20 -0700

    bumped version to 0.5.0, including support for connection and credential management

--- snap.rb
+++ snap.rb
@@ -2,8 +2,8 @@ class Snap < Formula
   desc "A command-line tool for managing snaps - workflows that connect triggers to actions"
   homepage "https://github.com/snapmaster-io/snap"
   url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap"
-  sha256 "3a613662109e533eace3005ff8e59470b9f37fed13fb41b712ab1ae113862cd8"
-  version "0.4.0"
+  sha256 "31b0cddef730e5d5ebb5e57f9743cc5f1c4dfa8831091c72a416a548cb9ae035"
+  version "0.5.0"
 
   bottle :unneeded
 
