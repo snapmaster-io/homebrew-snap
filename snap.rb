b3b8a952d92ac06f5f9ea1982267a34fa2d4dadf
Omri Gazitt <ogazitt@gmail.com>
2020-04-29 11:06:22 -0700

    bumped version to 0.5.1, which detects stale auth tokens and displays an error message

--- snap.rb
+++ snap.rb
@@ -2,8 +2,8 @@ class Snap < Formula
   desc "A command-line tool for managing snaps - workflows that connect triggers to actions"
   homepage "https://github.com/snapmaster-io/snap"
   url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap"
-  sha256 "31b0cddef730e5d5ebb5e57f9743cc5f1c4dfa8831091c72a416a548cb9ae035"
-  version "0.5.0"
+  sha256 "b87484846c542abc36f9681cd3113f093d649e51163125f6190d8e063bbd9657"
+  version "0.5.1"
 
   bottle :unneeded
 
