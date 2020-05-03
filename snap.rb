ebddfbf6486e4de6df07fbf1524a9bc8a1c7589d
Omri Gazitt <ogazitt@gmail.com>
2020-05-02 17:37:10 -0700

    updated SHA

--- snap.rb
+++ snap.rb
@@ -2,7 +2,7 @@ class Snap < Formula
   desc "A command-line tool for managing snaps - workflows that connect triggers to actions"
   homepage "https://github.com/snapmaster-io/snap"
   url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap-0.5.2.tar.gz"
-  sha256 "c6e4792a4a7a397568984acf441581125b46041be1bd2d69f144d5e817697724"
+  sha256 "ad5e6a9d694e4765166b905e5e2c97560cc52d886763a8f28e1fcbb98f1fbfeb"
   version "0.5.2"
 
   bottle :unneeded
--- stage.sh
+++ stage.sh
@@ -6,19 +6,20 @@ git rm archive/*
 # copy the compiled binary
 cp $GOPATH/src/github.com/snapmaster-io/snap/bin/snap ./archive
 
-# compute SHA
-SHA=$(shasum -a 256 archive/snap | awk '{printf $1}')
-
 # get version string
 VERSION=$(./archive/snap version |sed -e 's/.*<\(.*\)>,.*/\1/')
+BINARY=snap-$VERSION.tar.gz
 
 # compress the binary and name it using $VERSION, remove the uncompressed one
 cd archive 
-tar cvfz snap-$VERSION.tar.gz snap
+tar cvfz $BINARY snap
 rm snap
 cd ..
 
+# compute SHA
+SHA=$(shasum -a 256 archive/$BINARY | awk '{printf $1}')
+
 # print message
 echo SHA: $SHA
-echo archive binary: snap-$VERSION.tar.gz
+echo archive binary: $BINARY
 echo Please modify snap.rb with these two values before committing!
