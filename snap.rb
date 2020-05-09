a397624f8b770501f09dde927185f6be862a3948
Omri Gazitt <ogazitt@gmail.com>
2020-05-09 15:22:28 -0700

    bumped version in formula as well

--- snap.rb
+++ snap.rb
@@ -3,7 +3,7 @@ class Snap < Formula
   homepage "https://github.com/snapmaster-io/snap"
   url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap-0.5.3.tar.gz"
   sha256 "d64819dd4cab1bae0e1d1c35c5dc47913768df9e3066dc389285ccc21cf2936c"
-  version "0.5.2"
+  version "0.5.3"
 
   bottle :unneeded
 
