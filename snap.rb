c1cdd483bb76b86287d073cc4fe45e416daf4983
Omri Gazitt <ogazitt@gmail.com>
2020-06-10 23:01:52 -0700

    updated archive

--- snap.rb
+++ snap.rb
@@ -1,7 +1,7 @@
 class Snap < Formula
   desc "A command-line tool for managing snaps - workflows that connect triggers to actions"
   homepage "https://github.com/snapmaster-io/snap"
-  url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap-0.5.3.tar.gz"
+  url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap-0.5.4.tar.gz"
   sha256 "48c130f894bab8699edc799f5fc1323ab2453676e5b9ff9ee8468368dacc4a9f"
   version "0.5.4"
 
