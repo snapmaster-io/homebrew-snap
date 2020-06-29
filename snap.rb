ec7019fa81d6d32624df6b90563b8fca258ab311
Omri Gazitt <ogazitt@gmail.com>
2020-06-28 23:01:12 -0700

    updated formula to v0.5.5

--- snap.rb
+++ snap.rb
@@ -1,9 +1,9 @@
 class Snap < Formula
   desc "A command-line tool for managing snaps - workflows that connect triggers to actions"
   homepage "https://github.com/snapmaster-io/snap"
-  url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap-0.5.4.tar.gz"
-  sha256 "48c130f894bab8699edc799f5fc1323ab2453676e5b9ff9ee8468368dacc4a9f"
-  version "0.5.4"
+  url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap-0.5.5.tar.gz"
+  sha256 "0662034032114dfda79921b7e6ae3309407296c8367d329731ff063aceffad00"
+  version "0.5.5"
 
   bottle :unneeded
 
