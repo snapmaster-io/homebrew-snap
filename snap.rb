0ab6aa34050027b8a37bcdba28928b44b5c53c50
Omri Gazitt <ogazitt@gmail.com>
2020-07-26 22:30:02 -0700

    updated print pkg to reflect action output protocol; bumped version to 0.6.1

--- snap.rb
+++ snap.rb
@@ -1,9 +1,9 @@
 class Snap < Formula
   desc "A command-line tool for managing snaps - workflows that connect triggers to actions"
   homepage "https://github.com/snapmaster-io/snap"
-  url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap-0.6.0.tar.gz"
-  sha256 "0a5e243ad1f5f72abe32d897714b5b999944f44243c750ba12240fba22d9a0d8"
-  version "0.6.0"
+  url "https://github.com/snapmaster-io/homebrew-snap/raw/master/archive/snap-0.6.1.tar.gz"
+  sha256 "9a09a503d695d9c4e9838336e6448356a16ca1a4b3ec021c87986c89f4ec11d0"
+  version "0.6.1"
 
   bottle :unneeded
 
